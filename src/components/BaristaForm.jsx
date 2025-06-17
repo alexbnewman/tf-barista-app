f00fc9570e81389436afcfb96caa7adbcb336957
Alex Newman <alex02new@outlook.com>
2025-06-17 03:17:24 -0400

    b4 stretch features

--- src/components/BaristaForm.jsx
+++ src/components/BaristaForm.jsx
@@ -12,6 +12,11 @@ const BaristaForm = () => {
     const [currentDrink, setCurrentDrink] = useState('');
     const [trueRecipe, setTrueRecipe] = useState({});
 
+    const [correct_temp, setCheckedTemperature] = useState('');
+    const [correct_syrup, setCheckedSyrup] = useState('');
+    const [correct_milk, setCheckedMilk] = useState('');
+    const [correct_blended, setCheckedBlended] = useState('');
+
     const ingredients = {
         'temperature' : ['hot', 'lukewarm', 'cold'],
         'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
@@ -31,21 +36,50 @@ const BaristaForm = () => {
         'milk': '',
         'syrup': '',
         'blended': '' });
+
+        setCheckedTemperature('');
+        setCheckedSyrup('');
+        setCheckedMilk('');
+        setCheckedBlended('');
         
         getNextDrink();
     };
 
     const onCheckAnswer = () => {
-  
-};
+        if (trueRecipe.temp != inputs['temperature']){
+            setCheckedTemperature('wrong');
+        }
+        else {
+            setCheckedTemperature("correct");
+        }
+        if (trueRecipe.milk != inputs['milk']){
+            setCheckedMilk('wrong');
+        }
+        else {
+            setCheckedMilk("correct");
+        }
+        if (trueRecipe.syrup != inputs['syrup']){
+            setCheckedSyrup('wrong');
+        }
+        else {
+            setCheckedSyrup("correct");
+        }
+        if (trueRecipe.blended != inputs['blended']){
+            setCheckedBlended('wrong');
+        }
+        else {
+            setCheckedBlended("correct");
+        }
+    };
   return (
     <div>
         <h2>Hi, I'd like to order a:</h2>
         <div className="drink-container"><h2 className="mini-header">{currentDrink}</h2>
         <button type="new-drink-button" className="button newdrink" onClick={onNewDrink}>🔄</button></div>
-        <form>
+        <form className="container">
+            <div className="mini-container">
             <h3>Temperature</h3>
-                <div className="answer-space" >
+                <div className="answer-space" id={correct_temp}>
                 {inputs["temperature"]} 
                 </div>
                 <RecipeChoices
@@ -57,8 +91,10 @@ const BaristaForm = () => {
                 choices={ingredients["temperature"]}
                 checked={inputs["temperature"]}
             />
+            </div>
+            <div className="mini-container">
             <h3>Milk</h3>
-                <div className="answer-space" >
+                <div className="answer-space" id={correct_milk}>
                 {inputs["milk"]} 
                 </div>
                 <RecipeChoices
@@ -70,8 +106,10 @@ const BaristaForm = () => {
                 choices={ingredients["milk"]}
                 checked={inputs["milk"]}
             />
+            </div>
+            <div className="mini-container">
             <h3>Syrup</h3>
-                <div className="answer-space" >
+                <div className="answer-space" id={correct_syrup}>
                 {inputs["syrup"]} 
                 </div>
                 <RecipeChoices
@@ -83,8 +121,10 @@ const BaristaForm = () => {
                 choices={ingredients["syrup"]}
                 checked={inputs["syrup"]}
             />
-            <h3>Blendesd</h3>
-                <div className="answer-space" >
+            </div>
+            <div className="mini-container">
+            <h3>Blended</h3>
+                <div className="answer-space" id={correct_blended}>
                 {inputs["blended"]} 
                 </div>
                 <RecipeChoices
@@ -96,6 +136,7 @@ const BaristaForm = () => {
                 choices={ingredients["blended"]}
                 checked={inputs["blended"]}
             />
+            </div>
         </form>
         <button type='submit' className="button submit" onClick={onCheckAnswer}>
             Check Answer
